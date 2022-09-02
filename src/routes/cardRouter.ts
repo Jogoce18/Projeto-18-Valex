23e7de8e5877d63858def6f1ad112acae80a2eb2
johana <gomezjohana655@gmail.com>
2022-09-02 14:27:30 -0300

    feat:extrac card, block card,unlock card

--- src/routes/cardRouter.ts
+++ src/routes/cardRouter.ts
@@ -1,15 +1,19 @@
 import { Router } from "express";
 
-import { validNewCard,validCardActivation } from "../middlewares/cardMiddleware";
+import { validNewCard,validCardActivation,validBlockCard,validUnlockCard } from "../middlewares/cardMiddleware";
 import { checkApiKey } from "../middlewares/validateApikeyMiddleware";
-import { createCard,setCardPass} from "../controllers/cardController";
+import { createCard,setCardPass,blockCard,unlockCard,getExtract} from "../controllers/cardController";
 
-import { cardSchema,cardPasswordSchema } from "../schemas/cardSchema";
+import { cardSchema,cardPasswordSchema,blockCardSchema } from "../schemas/cardSchema";
 import { validateSchema } from "../middlewares/validateSchemaMiddleware";
 
 const cardRouter = Router();
 
 cardRouter.post("/newcard",validateSchema(cardSchema),checkApiKey,validNewCard,createCard); 
 cardRouter.post("/card/activate",validateSchema(cardPasswordSchema),validCardActivation,setCardPass); 
+cardRouter.get("/cards/:id"); 
+cardRouter.get("/card/:id/extract",getExtract); 
+cardRouter.put("/card/block",validateSchema(blockCardSchema),validBlockCard,blockCard);
+cardRouter.put("/card/unlock",validateSchema(blockCardSchema),validUnlockCard,unlockCard);
 
 export default cardRouter;
